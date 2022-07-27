d24bfca1b86c62680898eef762717bee88d17c1c
Agos95 <agostini.federico.95@gmail.com>
2022-07-27 16:49:01 +0200

    removed

--- python/base/minesweeper.py
+++ python/base/minesweeper.py
@@ -1,4 +1,5 @@
 # %%
+import argparse
 import random
 
 # %%
@@ -217,3 +218,28 @@ class MineSweeper():
         print(board_str)
 
 # %%
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--difficulty", choices=[0, 1, 2], default=0, type=int,
+                        help="Select difficulty:\n" +
+                        "\t0 -> Beginner     ( 9x9 , 10 Mines)" +
+                        "\t1 -> Intermediate (16x16, 40 Mines)" +
+                        "\t0 -> Advanced     (24x24, 99 Mines)")
+    return vars(parser.parse_args())
+
+# %%
+
+
+def main():
+    args = parse_args()
+    game = MineSweeper(args["difficulty"], fair=True)
+    game.play()
+
+
+# %%
+
+
+if __name__ == "__main__":
+    main()
