c42138dacf5c884df9e8efaa6d97abed9e8bd83b
Agos95 <agostini.federico.95@gmail.com>
2022-07-28 18:39:28 +0200

    add difficulty selector

--- javascript/minesweeper.js
+++ javascript/minesweeper.js
@@ -1,18 +1,76 @@
-let board = document.getElementById("board")
-let trueBoard = []
+let DIFFICULTY_LEVEL = {
+    0: {
+        "level": "Beginner",
+        "shape": [9, 9],
+        "mines": 10
+    },
+    1: {
+        "level": "Intermediate",
+        "shape": [16, 16],
+        "mines": 40
+    },
+    2: {
+        "level": "Advanced",
+        "shape": [24, 24],
+        "mines": 99
+    }
+}
 
-function MakeBoard(rows = 9, cols = 9) {
+
+let board = document.getElementById("board")
+let level = null
+let [rows, cols] = [null, null]
+let mines = null
+let trueBoard = null
+
+
+/**
+ * Gets the difficulty level by inspecting the radio selector.
+ */
+function GetDifficulty() {
+    let difficulty = null
+    let levels = document.getElementsByName("difficulty")
+    for (let x of levels) {
+        if (x.checked) {
+            difficulty = x.value
+            break
+        }
+    }
+    diff = DIFFICULTY_LEVEL[difficulty]
+    level = diff["level"]
+
+    rows = diff["shape"][0]
+    cols = diff["shape"][1]
+    mines = diff["mines"]
+    //console.log("rows = " + rows + " | cols = " + cols)
+    //console.log("Level = " + level)
+    //console.log("mines = " + mines)
+
+}
+
+/**
+ * Creates the boards for the game.
+ */
+function MakeBoard() {
+    // clear any previous element
     board.innerHTML = ""
-    // create the grid
+    trueBoard = []
+    // create the board
     for (let i = 0; i < rows; i++) {
         trueBoard.push([])
         let row = board.insertRow(i)
         for (let j = 0; j < cols; j++) {
-            trueBoard[i].push(false)
+            trueBoard[i].push(0)
             let cell = row.insertCell(j)
             cell.onclick = function () { ClickCell(this) }
         }
     }
+    console.log("trueBoard length = " + trueBoard.length)
+    console.log("trueBoard el. length = " + trueBoard[0].length)
+    //console.log("trueBoard = " + trueBoard.toString())
+
+    // Place Bombs
+
 }
 
 function ClickCell(cell) {
